fe4a289bfd585f6eeb9ab1b3ef4c4d3bb6f53a7a
Dika Afty <dikaafty@gmail.com>
2025-06-13 10:35:37 +0700

    Create Header component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,3 +1,7 @@
 import videobelajarLogo from "../assets/images/videobelajar-logo.png";
 import dehazeIcon from "../assets/images/dehaze-icon.png";
 import avatar from "../assets/images/avatar.png";
+
+const Header = () => {
+  
+}
