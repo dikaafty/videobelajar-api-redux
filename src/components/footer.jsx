9e4323c5fe32105b28baa1534ef42f4b66189779
Dika Afty <dikaafty@gmail.com>
2025-06-13 13:21:11 +0700

    Create footer jsx and its item in Footer component

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,10 @@
 const Footer = ({ children }) => {
   return (
-
+    <div 
+      className="w-full bg-white border-t-[1px] border-t-[#3A35411F] p-5 sm:py-15 sm:px-30
+      flex flex-col gap-4 sm:gap-5">
+        {children}
+    </div>
   );
 }
 
