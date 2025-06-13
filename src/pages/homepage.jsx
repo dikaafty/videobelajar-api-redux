aa05bfbc925a29acecb71bcca167bddabaf0c5d9
Dika Afty <dikaafty@gmail.com>
2025-06-13 20:31:02 +0700

    Fix useFetchCourse custom hook import location

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { useFetchCourse } from "./services/api/fetch-course";
+import { useFetchCourse } from "../services/api/fetch-course";
 import { setCourse } from "../features/user/course-slice";
 
 const Homepage = () => {
