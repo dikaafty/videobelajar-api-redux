ed49952f832fb571f4825caf3f976793f880728b
Dika Afty <dikaafty@gmail.com>
2025-06-13 19:21:22 +0700

    Declare courses state using useFetchCourse custom hook in Homepage component

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -2,6 +2,7 @@ import { useFetchCourse } from "./services/api/fetch-course";
 
 const Homepage = () => {
   const COURSE_API = import.meta.env.VITE_COURSE_API;
+  const { courses } = useFetchCourse(COURSE_API);
 
   return (
 
