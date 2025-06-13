19de6db3bffcb92c303b87dc1b3c00a62b65df84
Dika Afty <dikaafty@gmail.com>
2025-06-13 13:29:42 +0700

    Create nav and its items jsx in Nav component

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,6 +1,24 @@
 const Nav = ({ navText, navList = [] }) => {
   return (
+    <div className="flex flex-col gap-[15px]">
+      <p 
+        className="font-dm-sans font-bold text-base leading-[1.4] tracking-[0.2px] text-[#222325]
+        max-sm:flex max-sm:justify-between max-sm:gap-3 max-sm:after:content-['>'] 
+        max-sm:after:text-[#3A35418A]">
+          {navText}
+      </p>
 
+      <div className="hidden sm:flex sm:flex-col sm:gap-[13px]">
+        {
+          navList.length !== 0 && navList.map((nav) => (
+            <p key={id++} className="font-dm-sans font-medium text-base leading-[1.4] tracking-[0.2px] 
+            text-[#333333AD] cursor-pointer select-none hover:underline">
+              {nav}
+            </p>
+          ))
+        }
+      </div>
+    </div>
   );
 }
 
