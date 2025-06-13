33eb3d7e7742c91bac6004413b946fee49210425
Dika Afty <dikaafty@gmail.com>
2025-06-13 19:24:32 +0700

    Import setCourse reducer

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,12 +1,13 @@
 import { useEffect } from "react";
 import { useFetchCourse } from "./services/api/fetch-course";
+import { setCourse } from "../features/user/course-slice";
 
 const Homepage = () => {
   const COURSE_API = import.meta.env.VITE_COURSE_API;
   const { courses } = useFetchCourse(COURSE_API);
 
   useEffect(() => {
-    
+
   }, []);
 
   return (
