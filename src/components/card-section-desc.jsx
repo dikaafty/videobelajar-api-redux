60f34032392cd0595f834088fdaa00e40096bb20
Dika Afty <dikaafty@gmail.com>
2025-06-13 11:03:52 +0700

    Create card section desc and its items jsx in CardSectionDesc component

--- src/components/card-section-desc.jsx
+++ src/components/card-section-desc.jsx
@@ -1,6 +1,18 @@
 const CardSectionDesc = () => {
   return (
-    
+    <div className="w-fit flex flex-col gap-2.5">
+      <h3 
+        className="font-poppins text-2xl sm:text-[32px] font-semibold 
+          leading-[1.1] text-[#222325]">
+          Koleksi Video Pembelajaran Unggulan
+      </h3>
+
+      <p 
+        className="font-dm-sans text-sm sm:text-base font-medium leading-[1.4] 
+          tracking-[0.2px] text-[#333333AD]">
+          Jelajahi Dunia Pengetahuan Melalui Pilihan Kami!
+      </p>
+    </div>
   );
 }
 
