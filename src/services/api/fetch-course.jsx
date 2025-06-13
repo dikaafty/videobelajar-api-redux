a0854fb978cd6e31ce2b67ac0e95ca6006a5e4d9
Dika Afty <dikaafty@gmail.com>
2025-06-13 16:56:26 +0700

    Create login in useEffect hook to perform fetch data

--- src/services/api/fetch-course.jsx
+++ src/services/api/fetch-course.jsx
@@ -5,4 +5,20 @@ const useFetchCourse = (url) => {
   const [ courses, setCourses ] = useState([]);
   const [ error, setError ] = useState("");
   const [ isLoading, setIsLoading ] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(url);
+        const data = res.data;
+        setCourses(data);
+        setIsLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setIsLoading(false);
+      }
+    }
+
+    fetchData();
+  }, [url]);
 }
