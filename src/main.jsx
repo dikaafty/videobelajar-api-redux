95ec1ba2e57fd65bd3bd27e040a104d657b686e0
Dika Afty <dikaafty@gmail.com>
2025-06-13 14:57:28 +0700

    Import courseStore

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
+import { courseStore } from "./app/store.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
