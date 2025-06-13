bda14d7dce8a3092cc30a6f74b5c047f156b9590
Dika Afty <dikaafty@gmail.com>
2025-06-13 14:13:18 +0700

    Create courseSlice variable and its value

--- src/features/user/course-slice.jsx
+++ src/features/user/course-slice.jsx
@@ -1,1 +1,47 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const courseSlice = createSlice({
+  name: "course",
+  initialState: {
+    course: [],
+    selectedCourse: null,
+    courseImg: "",
+    courseTitle: "",
+    courseDescription: "",
+    coursePrice: "",
+    isOpen: false
+  },
+  reducers: {
+    setCourse: (state, action) => {
+      state.course = action.payload;
+    },
+    createNewCourse: (state, action) => {
+      state.course.push(action.payload);
+    },
+    handleDelete: (state, action) => {
+      state.course.filter(item => item !== action.payload);
+    },
+    setSelectedCourse: (state, action) => {
+      state.selectedCourse = action.payload;
+    },
+    handleEdit: (action) => {
+      setSelectedCourse(action.payload);
+      setIsOpen(true);
+    },
+    setCourseImg: (state, action) => {
+      state.courseImg = action.payload;
+    },
+    setCourseTitle: (state, action) => {
+      state.courseTitle = action.payload;
+    },
+    setCourseDescription: (state, action) => {
+      state.courseDescription = action.payload;
+    },
+    setCoursePrice: (state, action) => {
+      state.coursePrice = action.payload;
+    },
+    setIsOpen: (state, action) => {
+      state.isOpen = action.payload;
+    },
+  }
+});
