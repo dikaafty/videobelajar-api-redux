78539eb3b823fb613395c23d19b5c151d45810e1
Dika Afty <dikaafty@gmail.com>
2025-06-13 10:42:22 +0700

    Create body jsx and its item in Body component

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,6 +1,10 @@
 const Body = ({ children }) => {
   return (
-    
+    <div 
+      className="flex-grow bg-[#FFFDF3] py-7 px-5 sm:py-16 sm:px-30 flex flex-col justify-center 
+      items-center gap-6 sm:gap-16">
+        {children}
+    </div>
   );
 }
 
