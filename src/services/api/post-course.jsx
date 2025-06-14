2cd18c10abee00dc5813c83961064fa496befb76
Dika Afty <dikaafty@gmail.com>
2025-06-14 10:37:04 +0700

    Create post new course login in createPost function

--- src/services/api/post-course.jsx
+++ src/services/api/post-course.jsx
@@ -1,6 +1,13 @@
 import axios from "axios";
 import useRequestStates from "./request-states";
 
-const createPost = (newCourse) => {
-  
+const createPost = async (url, newCourse, setCourses, setError) => {
+  try {
+    const res = await axios.post(url, newCourse);
+    const data = res.data;
+    setCourses(data);
+    console.log("Post data successfully!");
+  } catch (err) {
+    setError(err.message);
+  }
 }
