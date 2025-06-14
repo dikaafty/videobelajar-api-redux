9efdc7f4e946d521c5e244ff6701d1306f3a8734
Dika Afty <dikaafty@gmail.com>
2025-06-14 19:33:48 +0700

    Change logic of setCourses setter in deleteCourse function

--- src/services/api/delete-course.jsx
+++ src/services/api/delete-course.jsx
@@ -4,7 +4,7 @@ const deleteCourse = async (url, idx, setCourses, setError) => {
   try {
     const res = await axios.delete(`${url}/${idx}`);
     const data = res.data;
-    setCourses(data);
+    setCourses(prev => prev.filter(course => course.id !== idx));
   } catch (err) {
     setError(err.message);
   }
