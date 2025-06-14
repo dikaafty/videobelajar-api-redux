e047b1982ba53409a38ded7d0d5e67cd1391220f
Dika Afty <dikaafty@gmail.com>
2025-06-14 14:16:22 +0700

    Setup Redux dispatch in CourseActionBtns component

--- src/components/course-action-btns.jsx
+++ src/components/course-action-btns.jsx
@@ -2,6 +2,8 @@ import { handleEdit, handleDelete } from "../features/user/course-slice";
 import { useDispatch } from "react-redux";
 
 const CourseActionBtns = ({ action }) => {
+  const dispatch = useDispatch();
+
   return (
 
   );
