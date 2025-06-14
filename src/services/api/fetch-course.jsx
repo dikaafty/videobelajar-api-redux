90c2c08d275a8bb66932358527ddbe97008a5b07
Dika Afty <dikaafty@gmail.com>
2025-06-14 08:28:52 +0700

    Remove all states in useFetchCourse custom hook

--- src/services/api/fetch-course.jsx
+++ src/services/api/fetch-course.jsx
@@ -2,10 +2,6 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export const useFetchCourse = (url) => {
-  const [ courses, setCourses ] = useState([]);
-  const [ error, setError ] = useState("");
-  const [ isLoading, setIsLoading ] = useState(true);
-
   useEffect(() => {
     const fetchData = async () => {
       try {
