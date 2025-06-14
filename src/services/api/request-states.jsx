38f78d38ec13a2a653516b0b4ccdc8d99f301346
Dika Afty <dikaafty@gmail.com>
2025-06-14 08:30:57 +0700

    Create courses, error and isLoading states in useRequestStates custom hook

--- src/services/api/request-states.jsx
+++ src/services/api/request-states.jsx
@@ -1,3 +1,5 @@
 const useRequestStates = () => {
-  
+  const [ courses, setCourses ] = useState([]);
+  const [ error, setError ] = useState("");
+  const [ isLoading, setIsLoading ] = useState(true);
 }
