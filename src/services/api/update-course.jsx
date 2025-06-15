6cfe5c4c0abad066ce32285e894087b306c5dffa
Dika Afty <dikaafty@gmail.com>
2025-06-15 13:45:31 +0700

    Create logic for updating in updateCourse function

--- src/services/api/update-course.jsx
+++ src/services/api/update-course.jsx
@@ -1,7 +1,13 @@
 import axios from "axios";
 
-const updateCourse = (url, item, itemId, setCourses, setError) => {
-  
+const updateCourse = async (url, item, itemId, setCourses, setError) => {
+  try {
+    const res = await axios.put(`${url}/${itemId}`, item);
+    const data = res.data;
+    setCourses(data);
+  } catch (err) {
+    setError(err.message);
+  }
 }
 
 export default updateCourse;
