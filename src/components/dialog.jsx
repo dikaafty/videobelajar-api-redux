d16a0d054facaf57c047dc7bb6ba6126a4c5c60e
Dika Afty <dikaafty@gmail.com>
2025-06-15 14:06:04 +0700

    Import setEditedCourse reducer

--- src/components/dialog.jsx
+++ src/components/dialog.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
    setCourse, createNewCourse, setCourseImg, setCourseTitle, setCourseDescription,
-   setCoursePrice, setSelectedCourse, setIsOpen
+   setCoursePrice, setSelectedCourse, setIsOpen, setEditedCourse
   } 
 from "../features/user/course-slice";
 import useRequestStates from "../services/api/request-states";
