74e43820760aa5330f98c6ded6bfa1d6c22f1492
Dika Afty <dikaafty@gmail.com>
2025-06-22 20:25:43 +0700

    Delete multiple conditionals of ternary operator to handle isLoading and error states and display fetched courses data from API server in Homepage component

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -42,36 +42,7 @@ const Homepage = () => {
               <Card id={8} />
               <Card id={9} />
 
-              {
-                isLoading ? 
-                (
-                  <p 
-                    className="font-poppins text-2xl sm:text-[32px] font-semibold leading-[1.1]
-                    text-[#222325]">
-                      Loading...
-                  </p>
-                ) : error ? 
-                (
-                  <p 
-                    className="font-poppins text-2xl sm:text-[32px] font-semibold leading-[1.1] 
-                    text-red-500">
-                      Error: {error}
-                  </p>
-                ) : (
-                  course.courseData.map((item, idx) => 
-                    <Fragment key={idx}>
-                      <Card isUserCourse={true} idx={idx}>
-                        <Container 
-                          className="w-full flex justify-end sm:justify-around max-sm:col-span-2 
-                          max-sm:gap-2 *:max-sm:mt-1.5">
-                            <CourseActionsBtn action="edit" item={item} itemId={item.id} />
-                            <CourseActionsBtn action="delete" idx={idx} itemId={item.id} />
-                        </Container>
-                      </Card>
-                    </Fragment>
-                  )
-                )
-              }
+              
           </Container>
         </Container>
 
