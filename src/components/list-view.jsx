3d9a9deaf494c6270a9db535019a33fc871f0b4f
Dika Afty <dikaafty@gmail.com>
2025-06-22 20:12:05 +0700

    Import setCourse reducer

--- src/components/list-view.jsx
+++ src/components/list-view.jsx
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, Fragment } from "react";
 import { useFetchCourse } from "../services/api/fetch-course";
+import { setCourse } from "../features/user/course-slice";
 import Card from "./card";
 import Container from "./container";
 import CourseActionsBtn from "./course-actions-btn";
