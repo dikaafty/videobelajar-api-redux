d6e5a73eb8fd0023f333d0f7edb916ad0a22ba52
Dika Afty <dikaafty@gmail.com>
2025-06-22 20:10:02 +0700

    Import course api url

--- src/components/list-view.jsx
+++ src/components/list-view.jsx
@@ -1,3 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, Fragment } from "react";
 import { useFetchCourse } from "../services/api/fetch-course";
+
+const COURSE_API = import.meta.env.VITE_COURSE_API;
