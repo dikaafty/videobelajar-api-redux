8d555c6f0b922b90affa44ba4bd0073854a256af
Dika Afty <dikaafty@gmail.com>
2025-06-22 20:10:36 +0700

    Import Card component

--- src/components/list-view.jsx
+++ src/components/list-view.jsx
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, Fragment } from "react";
 import { useFetchCourse } from "../services/api/fetch-course";
+import Card from "./card";
 
 const COURSE_API = import.meta.env.VITE_COURSE_API;
